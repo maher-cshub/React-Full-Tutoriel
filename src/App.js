ae81345f349a97386f59d14e2786ae9e87b48070
maher-cshub <maherarab.1299@gmail.com>
2023-11-21 10:25:45 +0100

    tailwindcss working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-=======
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
@@ -9,16 +5,13 @@ import Footer from "./components/Footer/Footer";
 import Main from "./routes/Main/Main";
 import About from "./routes/About/About";
 import PokemonInfo from "./routes/Pokemon/PokemonInfo";
->>>>>>> states_events_&_mini_project
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="#" className="App-logo" alt="logo" />
       </header>
-=======
       <Router>
         <Header></Header>
 
@@ -30,7 +23,6 @@ function App() {
 
         <Footer></Footer>
       </Router>
->>>>>>> states_events_&_mini_project
     </div>
   );
 }
