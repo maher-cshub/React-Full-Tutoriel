8f08ea05aa674b06932249fba9e7c7940e3653fc
maher-cshub <maherarab.1299@gmail.com>
2023-11-21 10:45:32 +0100

    setu flowbite for react

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/*
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
@@ -19,6 +20,35 @@ function App() {
           <Route path="/" exact Component={Main} />
           <Route path="/about" Component={About} />
           <Route path="/:pokeName" Component={PokemonInfo} />
+        </Routes>
+
+        <Footer></Footer>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
+*/
+
+//mini store project
+
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./mini_store_project/components/Header/Header";
+import Footer from "./mini_store_project/components/Footer/Footer";
+import Main from "./mini_store_project/routes/Main/Main";
+import About from "./mini_store_project/routes/About/About";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header></Header>
+
+        <Routes>
+          <Route path="/" exact Component={Main} />
+          <Route path="/about" Component={About} />
         </Routes>
 
         <Footer></Footer>
